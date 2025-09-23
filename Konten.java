919e9bec7f10b1a6afb0d2e96c3772353a7809d2
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-23 07:47:30 +0700

    add Class Konten - Construktor - private atribut - method set & getter namaKonten

--- Konten.java
+++ Konten.java
@@ -1,3 +1,20 @@
 public class Konten {
-    
+    private String namaKonten;
+    private Video video;
+    private Artikel artikel;
+    private Kuis kuis;
+
+    Konten(String namaKonten , Video video , Artikel artikel , Kuis kuis){
+        this.namaKonten = namaKonten;
+        this.video = video;
+        this.artikel = artikel;
+        this.kuis = kuis;
+    }
+
+    public void setNamaKonten(String namaKonten) {
+        this.namaKonten = namaKonten;
+    }
+    public String getNamaKonten() {
+        return namaKonten;
+    }
 }
