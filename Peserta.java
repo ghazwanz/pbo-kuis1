7a9c93d0fb57f4cfc46fc7bb17e63dbbc3ce874e
ghazwanz <ababilghazwan@gmail.com>
2025-09-23 15:57:37 +0700

    Progress class user & peserta

--- Peserta.java
+++ Peserta.java
@@ -1,3 +1,7 @@
-public class Peserta {
+public class Peserta extends User {
+
+    public Peserta(String id, String name, String email){
+        super(id,name,email);
+    }
     
 }
--- User.java
+++ User.java
@@ -1,6 +1,26 @@
 public class User {
-    private String nama;
-    public User(){}
+    private String id;
+    private String name;
+    private String email;
+    public User(String id, String name, String email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
-    
+    public String getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void infoUser(){
+        System.out.printf("ID      : %s\n",id);
+        System.out.printf("Nama    : %s\n",name);
+        System.out.printf("Email   : %s\n",email);
+    }
 }
