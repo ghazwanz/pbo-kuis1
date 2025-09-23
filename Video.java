1192c8f376948c046d083e362047a4f949f35d59
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-23 07:46:30 +0700

    add Class Video - Construktor - Method Set and getter atribut - Method infoVideo()

--- Video.java
+++ Video.java
@@ -1,3 +1,36 @@
 public class Video {
-    
+    private String judulVideo;
+    private String resolusi;
+    private Konten konten;
+
+    Video(String judulVideo , String resolusi){
+        this.judulVideo = judulVideo;
+        this.resolusi =  resolusi;
+    }
+
+    public void setJudulVideo(String judulVideo) {
+        this.judulVideo = judulVideo;
+    }
+    public String getJudulVideo() {
+        return judulVideo;
+    }
+
+    public void setResolusi(String resolusi) {
+        this.resolusi = resolusi;
+    }
+    public String getResolusi() {
+        return resolusi;
+    }
+
+    public String infoVideo(){
+        String info ="";
+        info += "Judul Video : "+judulVideo+"\n";
+        info += "+-----------------------+";
+        info += "|                       |";
+        info += "|  {"+ konten.getNamaKonten()+"}; |";
+        info += "|                       |";
+        info += "+-----------------------+";
+        info += "Resolusi : "+resolusi+"\n";
+        return info;
+    }
 }
