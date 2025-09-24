bc7c1399b4dc97d89473799a329a4763fb382f3c
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-24 08:42:05 +0700

    update class video sementara

--- Video.java
+++ Video.java
@@ -1,36 +1,29 @@
-public class Video {
+public class Video extends Konten{
     private String judulVideo;
     private String resolusi;
-    private Konten konten;
-
-    Video(String judulVideo , String resolusi){
-        this.judulVideo = judulVideo;
-        this.resolusi =  resolusi;
+    
+    public void setjudulVideo(String newValue){
+        judulVideo = newValue;
     }
-
-    public void setJudulVideo(String judulVideo) {
-        this.judulVideo = judulVideo;
-    }
-    public String getJudulVideo() {
-        return judulVideo;
-    }
-
     public void setResolusi(String resolusi) {
         this.resolusi = resolusi;
     }
-    public String getResolusi() {
-        return resolusi;
+
+    public void infoVideo(){
+        super.infoKonten();
+        System.out.println("Judul Video : " + judulVideo);
+        System.out.println("Resolusi Video : "+ resolusi);
     }
 
-    public String infoVideo(){
-        String info ="";
-        info += "Judul Video : "+judulVideo+"\n";
-        info += "+-----------------------+";
-        info += "|                       |";
-        info += "|  {"+ konten.getNamaKonten()+"}; |";
-        info += "|                       |";
-        info += "+-----------------------+";
-        info += "Resolusi : "+resolusi+"\n";
-        return info;
-    }
+    // public String infoVideo(){
+    //     String info ="";
+    //     info += "Judul Video : "+judulVideo+"\n";
+    //     info += "+-----------------------+";
+    //     info += "|                       |";
+    //     info += "|  {"+ super.getNamaKonten()+"}; |";
+    //     info += "|                       |";
+    //     info += "+-----------------------+";
+    //     info += "Resolusi : "+resolusi+"\n";
+    //     return info;
+    // }
 }
