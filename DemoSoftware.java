5f441c8c25f0373ab51c0d309682f1216460ddeb
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-24 14:21:14 +0700

    update demo

--- DemoSoftware.java
+++ DemoSoftware.java
@@ -1,37 +1,8 @@
 import java.util.Scanner;
 
 public class DemoSoftware {
-
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
-        Kursus menu = new Kursus();
-        int pilihan;
-        
-        do {
-            System.out.println("== LOGIN USER ==");
-            System.out.println("1. Murid \n2. Instruktur \n0. Exit");
-            System.out.print("Pilih menu: ");
-            pilihan = sc.nextInt();
-            switch (pilihan) {
-                case 1:
-                    menu.menuKursus();
-                    break;
-                case 2:
-                    // menu.menuInstrukur();
-                    break;
-                case 0:
-                    System.out.println("Bye!");
-
-                    return;
-            
-                default:
-                    System.out.println("Pilih yang bener Kocag!\n");
-
-            }
-        } while (true);
-
+        Software app = new Software();
+        app.run();
     }
 }
-
-
