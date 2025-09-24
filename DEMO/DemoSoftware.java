44134babdea6571e65660cb7f5074de05beda758
fitraadhim <azzazria123@gmail.com>
2025-09-24 14:24:03 +0700

    deklarasi

--- DEMO/DemoSoftware.java
+++
@@ -1,9 +0,0 @@
-package DEMO;
-import java.util.Scanner;
-
-public class DemoSoftware {
-    public static void main(String[] args) {
-        Software app = new Software();
-        app.run();
-    }
-}
