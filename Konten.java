6290c3c800246e4c36b2c7b7dcb3b312daa56316
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-24 14:09:07 +0700

    update Konten

--- Konten.java
+++ Konten.java
@@ -1,26 +1,27 @@
-public class Konten {
+public abstract class Konten {
     private String idKonten;
     private String judulKonten;
-    
-    // public Konten(String judulKonten ){
-    //     this.judulKonten = judulKonten;
-    // }
+    private String namaKonten;
 
-    public void setIdKonten(String idKonten) {
-        this.idKonten = idKonten;
-    }
     public String getIdKonten() {
         return idKonten;
     }
-
-    public void setjudulKonten(String judulKonten) {
-        this.judulKonten = judulKonten;
+    public void setIdKonten(String idKonten) {
+        this.idKonten = idKonten;
     }
-    public String getjudulKonten() {
+    public String getJudulKonten() {
         return judulKonten;
     }
+    public void setJudulKonten(String judulKonten) {
+        this.judulKonten = judulKonten;
+    }
+    public String getNamaKonten() {
+        return namaKonten;
+    }
+    public void setNamaKonten(String namaKonten) {
+        this.namaKonten = namaKonten;
+    }
 
-    public void infoKonten(){
-        System.out.println("Konten : "+judulKonten);
-    } 
+    // Metode abstrak yang harus diimplementasikan oleh kelas turunan
+    public abstract void infoKonten();
 }
