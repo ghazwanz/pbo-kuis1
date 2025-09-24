cb98f81460a8450ab2ae56c87009d49c5076b922
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-24 14:12:39 +0700

    add artikel

--- Artikel.java
+++ Artikel.java
@@ -1,3 +1,18 @@
-public class Artikel {
-    
+public class Artikel extends Konten {
+    private int jumlahHalaman;
+
+    public void setJumlahHalaman(int jumlahHalaman) {
+        this.jumlahHalaman = jumlahHalaman;
+    }
+
+    public int getJumlahHalaman() {
+        return jumlahHalaman;
+    }
+
+    @Override
+    public void infoKonten() {
+        System.out.println("Tipe Konten    : Artikel");
+        System.out.println("Judul Artikel  : " + getJudulKonten());
+        System.out.println("Jumlah Halaman : " + jumlahHalaman);
+    }
 }
