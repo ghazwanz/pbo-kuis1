e5030582207f87cc34e83ec0c5671aba834dcc1f
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-24 07:42:03 +0700

    add Scanner

--- DemoSoftware.java
+++ DemoSoftware.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class DemoSoftware {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+        sc.close();
+    }
 }
