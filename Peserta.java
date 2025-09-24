26e5f4ccef5dbe21e12d132b0e551785e69e8db2
fitraadhim <azzazria123@gmail.com>
2025-09-24 14:16:34 +0700

    done

--- Peserta.java
+++ Peserta.java
@@ -1,7 +1,32 @@
-public class Peserta extends User {
+import java.util.ArrayList;
+import java.util.List;
 
-    public Peserta(String id, String name, String email){
-        super(id,name,email);
+public class Peserta extends User {
+    private List<Kursus> kursusDiambil;
+
+    public Peserta(String id, String name, String email) {
+        super(id, name, email);
+        this.kursusDiambil = new ArrayList<>();
     }
-    
+
+    public void daftarKursus(Kursus kursus) {
+        kursusDiambil.add(kursus);
+        System.out.println(getName() + " berhasil mendaftar kursus " + kursus.getNamaKursus());
+    }
+
+    public List<Kursus> getKursusDiambil() {
+        return kursusDiambil;
+    }
+
+    public void infoPeserta() {
+        super.infoUser();
+        System.out.println("Kursus yang diambil:");
+        if (kursusDiambil.isEmpty()) {
+            System.out.println("  Belum mengambil kursus apapun.");
+        } else {
+            for (Kursus k : kursusDiambil) {
+                System.out.println("  - " + k.getNamaKursus());
+            }
+        }
+    }
 }
